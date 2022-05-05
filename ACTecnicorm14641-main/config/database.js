345d6454f9a005b007020b2f9e16db88835a5666
Henrique Khouri <103910182+12345-oss@users.noreply.github.com>
2022-05-05 12:55:15 -0300

    Delete ACTecnicorm14641-main directory

--- ACTecnicorm14641-main/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const conexao = async()=>{
-    var atlas = await mongoose.connect('mongodb+srv://UserAdmin:1234567890@ac1tri.aganv.mongodb.net/ac1tri')
-}
-
-module.exports = conexao
